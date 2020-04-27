fb60de850a37af01cd0471d306755de5040f3802
thibaud-leroy <thibaud.leroy98@gmail.com>
2020-04-27 15:31:03 +0200

    df

--- script.js
+++ script.js
@@ -3,59 +3,105 @@ const config = {
      height :600,
      type : Phaser.AUTO,
      physics: {
-            default : 'arcade',
-            arcade : {
-                gravity: {y : 0}
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0},
+            debug: false
         }
-     },
+    },                 
      input: {
         gamepad: true
-     },
-
+            },
      scene: {
          preload : preload,
          create  : create,
          update  : update
-     }
+            }
 }
-
 
 const game = new Phaser.Game(config)
 let billy
 let cursors
 let world
-
+let arbre
+let enemy
+let Contour
 
 
 function preload ()  {
-    // Chargement des assets
+    //-----------------Chargement des assets------------------   
+    
     //Personnage
-    this.load.image('billyLD',  'assets/BillyLDs.png')// Regarde en bas BillyLDs
-
+    let BillyLD   = this.load.image('billyLD' ,  'assets/BillyLD.png' )      // Regarde en bas
+    let BillyLT   = this.load.image('billyLT' ,  'assets/BillyLT.png' )      // Regarde en haut
+    let BillyLL   = this.load.image('billyLL' ,  'assets/BillyLL.png' )      // Regarde à gauche
+    let BillyLR   = this.load.image('billyLR' ,  'assets/BillyLR.png' )      // Regarde à droite
+    let BillyWL   = this.load.image('billyWL' ,  'assets/BillyWL.png' )      // Marche à gauche
+    let BillyWR   = this.load.image('billyWR' ,  'assets/BillyWR.png' )      // Marche à droite
+    let BillyWD   = this.load.image('billyWD' ,  'assets/BillyWD.png' )      // Marche vers le bas  
+    let BillyWT   = this.load.image('billyWT' ,  'assets/BillyWT.png' )      // Marche vers le haut  
+    let BillyWD1  = this.load.image('billyWD1',  'assets/BillyWD1.png')     // Marche vers le bas   
+    let BillyWT1  = this.load.image('billyWT1',  'assets/BillyWT1.png' )      // Marche vers le haut  
+    
+    this.load.spritesheet('dude','assets/billy_sprite.png',{frameWidth : 32, frameHeight : 48})
     //Monde
     this.load.image('monde0',  'assets/monde0.png')
-    this.load.image('contour',  'assets/monde1.png')
+    Contour = this.load.image('contour', 'assets/monde1.png')
+    this.load.image('arbre',  'assets/Arbre.png')
 
     //Ennemis
+    this.load.image('enemy', 'assets/enemy.png')
+  
     //Items
 }
+
+
 function create  ()  {
-    world = this.add.image(400, 300,'monde0')
-    //Ajout de la carte
-    this.add.image(400, 300,'monde0')//zone jouable
-   // this.add.image(400, 300,'monde1')//zone de détourage
+    const dude = this.add.sprite(100,100,'dude',0);
+    this.anims.create({
+        key :'walk',
+        repeeat : -1,
+        frameRate : 5,
+        frames : this.anims.generateFrameNames('dude',{start : 1, end:3})
+    });
+    dude.play('walk');
+
+
+
+
+//------ DEBUG SOURIS-----------
+    let x = this.input.x;
+    let y = this.input.y;
+    this.input.on('pointerup', function(data) {   
+        console.log(data.upX, data.upY);
+    },this)
+
+
+    world = this.add.image(400, 300,'monde0')//Ajout de la carte
+    this.add.image(400, 300,'contour')//zone non jouable
+    
     
     //Ajout du personnage
-    billy = this.physics.add.image(500, 200, 'billyLD')
+    billy = this.physics.add.image(500, 200, 'billyLD').setScale(0.5);
+    enemy = this.physics.add.image(20,20,'enemy').setScale(.5);
+    console.log(enemy)
+    arbre = this.add.image(652,155,'arbre');
+
     billy.body.collideWorldBounds = true;
     cursors =this.input.keyboard.createCursorKeys()
 
-    this.add.image(400,300,'contour');
-    contour = this.physics.add.staticGroup();
 
+    //rectangle rivière + arbre zone 2 & 3 
+    // x 0 y 76
+    // x 511 y 12
     
 }
+
 function update  ()  {
+  
+
+//-------------Déplacements du personnage jouable-----------
+  
 
     billy.setVelocity(0)
     if (cursors.up.isDown){
@@ -74,30 +120,8 @@ function update  ()  {
         console.log('touche droite appuyé')
         billy.setVelocity(100,0)
     }
+ 
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- /*this.load.image('billyLR',  'assets/BillyLR.png')// Regarde à droite
-    this.load.image('billyLL',  'assets/BillyLL.png')// Regarde à gauche
-    this.load.image('billyLT',  'assets/BillyLT.png')// Regarde en haut
-    this.load.image('billyWD',  'assets/BillyWD.png')//Marche en bas
-    this.load.image('billyWD1',  'assets/BillyWD1.png')//Marche en bas
-    this.load.image('billyWL',  'assets/BillyWL.png')//Marche a gauche
-    this.load.image('billyWR',  'assets/BillyWR.png')//Marche a droite
-    this.load.image('billyWT1',  'assets/BillyWT1.png')//Marche en haut
-    this.load.image('billyWT',  'assets/BillyWT.png')//Marche en haut
-    this.load.image('billyHit',  'assets/BillyHit.png')//Dégats
-    this.load.image('billyHit1',  'assets/BillyHit1.png')*/
+//-------------Déplacements des personnages non jouables-----------
